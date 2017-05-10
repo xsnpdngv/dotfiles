8118205b3a4f4fb93afd7c8cdbc657ad1fae56db
Tamas Dezso <dezso.t.tamas@gmail.com>
2017-05-10 12:48:15 +0200

    Generalize plugin installation

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # ==========================================================================
 # @file    vinstall.sh
 # @brief   shell script to install bash profile, Vim config and plugins
@@ -12,22 +12,28 @@ cp -i .bash* ~/
 # copy .vimrc and .gvimrc
 cp -i .vimrc .gvimrc ~/
 
-# install plugin autoloader: pathogen
+# set Vim configuration location and plugins to be installed
+VIM=$HOME/.vim
+PLUGINS=(
+    "https://github.com/scrooloose/nerdtree"              # file tree explorer
+    "https://github.com/tomasr/molokai"                   # dark color scheme
+    "https://github.com/octol/vim-cpp-enhanced-highlight" # C++ higlight
+)
+
+# install plugin autoloader pathogen (if not yet present)
 # see: https://github.com/tpope/vim-pathogen
 # remember to put `pathogen#infect()` to .vimrc to have plugins autoloaded
-if [ ! -f ~/.vim/autoload/pathogen.vim ]
+if [ ! -f $VIM/autoload/pathogen.vim ]
 then
     # check if curl is available
     command -v curl >/dev/null || { echo "Missing curl. Aborting."; exit 1; }
     # create dirs, download
-    mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+    mkdir -p $VIM/autoload $VIM/bundle && \
+    curl -LSso $VIM/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 fi
 
-# install NERDTree plugin
-[ -d ~/.vim/bundle/nerdtree ] || \
-git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
-
-# install Molokai color scheme
-[ -d ~/.vim/bundle/molokai ] || \
-git clone https://github.com/tomasr/molokai ~/.vim/bundle/molokai
+# install plugins by cloning them from github (if not yet cloned)
+for PLUGIN in "${PLUGINS[@]}"; do
+    DESTDIR=$VIM/bundle/${PLUGIN##*\/}
+    [ -d "$DESTDIR" ] || git clone $PLUGIN $DESTDIR
+done
